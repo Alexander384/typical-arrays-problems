e5738bebd0d822691ff65572304304d2cb1a23ac
Alexander384 <salukalex@mail.ru>
2021-08-16 01:09:14 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0;
+    let resMin = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (resMin > array[i]) resMin = array[i];
+    }
+    return resMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0;
+  let resMax = array[0];
+  for (let i = 1; i < array.length; i++) {
+      if (resMax < array[i]) resMax = array[i];
+  }
+  return resMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0) return 0;
+    return array.reduce((a, b) => (a + b)) / array.length;
 }
+
+
